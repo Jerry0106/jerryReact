9e28fa0cee952de42cfeca19e4f70ff3991c8115
Chenyenjui <capricorn15.tw@gmail.com>
2022-04-22 18:22:19 +0800

    Weather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import Parent from './components/Parent'
 import SpeedConverter from './components/SpeedConverter'
 import React from 'react'
+import WeatherApp from './components/WeatherApp'
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       {/* <Parent /> */}
       {/* <Test10 /> */}
       {/* <div id="root"></div> */}
-      <SpeedConverter />
+      {/* <SpeedConverter /> */}
+      <WeatherApp />
     </>
   )
 }
